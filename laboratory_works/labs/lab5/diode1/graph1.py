4e3c661a70a7c73c7e15d5c49945875df03f118c
Sergey <s.remez-me23@lll.kpi.ua>
2021-12-23 21:27:47 +0200

    23.12.2021 21:30

--- laboratory_works/labs/lab5/diode1/graph1.py
+++ laboratory_works/labs/lab5/diode1/graph1.py
@@ -1,23 +1,23 @@
 import matplotlib.pyplot as plt
-import library.plotio as peg
-import library.xlsxio as io
+import laboratory_works.library.plotio as peg
+import laboratory_works.library.xlsxio as io
 
 index1 = 0
-index2 = 2
+index2 = 1
 
 columns = [[chr(i) for i in range(ord('A'), ord('F')+1)]] * 4
 rows = [[2, 22], [2, 22], [2, 30], [2, 22]]
 
 
 # all_sheets = io.get_all_sheets_from_file('../../data/lab5/dataBron.xlsx', columns, rows)
-all_sheets = io.get_all_sheets_from_file('../../../data/lab5/data.xlsx', columns, rows)
+all_sheets = io.get_all_sheets_from_file('../!data/data.xlsx', columns, rows)
 peg.get_graph_with_errors(plt, all_sheets[index1][2], all_sheets[index1][4], all_sheets[index1][3],
                           all_sheets[index1][5], 2, 0.5, 2, 'k', True, f'Ud, В', f'Id, мА')
 peg.get_graph_with_errors(plt, all_sheets[index2][2], all_sheets[index2][4], all_sheets[index2][3],
                           all_sheets[index2][5], 2, 0.5, 2, 'k', False, f'Ud, В', f'Id, мА')
-plt.plot([0, 1.5], [0, 0], color='k')
-plt.plot([0, 0], [0, 20], color='k')
-plt.title('Діоди 1 та 2 - прямі гілки')
+plt.plot([-0.5, 1.5], [0, 0], color='k')
+plt.plot([0, 0], [-20, 20], color='k')
+plt.title('Діод 1 - ВАХ')
 plt.grid()
 
 
--- laboratory_works/labs/lab5/diode2/graph1.py
+++ laboratory_works/labs/lab5/diode2/graph1.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-import library.plotio as peg
-import library.xlsxio as io
+import laboratory_works.library.plotio as peg
+import laboratory_works.library.xlsxio as io
 
 index1 = 2
 index2 = 3
@@ -12,7 +12,7 @@ rows = [[2, 22], [2, 22], [2, 30], [2, 22]]
 
 
 # all_sheets = io.get_all_sheets_from_file('../../data/lab5/dataBron.xlsx', columns, rows)
-all_sheets = io.get_all_sheets_from_file('../../../data/lab5/data.xlsx', columns, rows)
+all_sheets = io.get_all_sheets_from_file('../!data/data.xlsx', columns, rows)
 peg.get_graph_with_errors(plt, all_sheets[index1][2], all_sheets[index1][4], all_sheets[index1][3],
                           all_sheets[index1][5], 2, 0.5, 2, 'k', True, f'Ud, мВ', f'Id, мА')
 peg.get_graph_with_errors(plt, np.array(all_sheets[index2][2]) * 0.001, all_sheets[index2][4],
